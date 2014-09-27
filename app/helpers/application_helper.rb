f9df07e969b5bfa0eaf1c56bb6b0fd4d2ee5c65a
Boris Kuznetsov <achempion@gmail.com>
2014-09-27 21:13:22 +0400

    helper for status of build

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def build_status build
+    css_class = 'label'
+    css_class << ' label-green' if build.success?
+    css_class << ' label-red' if build.fail?
+
+    content_tag(:div, build.status, class: css_class)
+  end
+
 end
