9d42e8db75c1f60bea917a58c86dacdbacbde362
Boris Kuznetsov <achempion@gmail.com>
2014-10-02 20:01:02 +0400

    fix for project without builds

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -2,27 +2,37 @@ require 'rails_helper'
 
 describe 'projects' do
 
-  before do
-    create_project('repo/project1', 'secret')
-    create_project('repo/project2', 'secret')
-
-    visit projects_path
-  end
-
-  context 'displayed' do
-    it do
-      expect(page).to have_content('repo/project1')
-      expect(page).to have_content('repo/project2')
-    end
-  end
-
-  context 'remove one' do
-    let(:project2) { Project.find_by!(path: 'repo/project2') }
-
+  context 'with builds' do
     before do
-      find("a[href='/projects/#{project2.id}'][data-method='delete']").click
+      create_project('repo/project1', 'secret')
+      create_project('repo/project2', 'secret')
+
+      visit projects_path
     end
 
-    it { expect(page).to_not have_content('repo/project2') }
+    context 'displayed' do
+      it do
+        expect(page).to have_content('repo/project1')
+        expect(page).to have_content('repo/project2')
+      end
+    end
+
+    context 'remove one' do
+      let(:project2) { Project.find_by!(path: 'repo/project2') }
+
+      before do
+        find("a[href='/projects/#{project2.id}'][data-method='delete']").click
+      end
+
+      it { expect(page).to_not have_content('repo/project2') }
+    end
+  end
+
+  context 'without builds' do
+    before { create(:project) }
+
+    context 'not raise errors' do
+      it { expect { visit projects_path }.to_not raise_error }
+    end
   end
 end
