0d8c355b6a0265bb9e7b5658a29fa612c6cc3263
Boris Kuznetsov <achempion@gmail.com>
2014-10-14 22:18:02 +0400

    build runner spec

---
+++ spec/services/scheduler_service/build_runner_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe SchedulerService::BuildRunner do
+  let!(:build) { create(:build) }
+
+  describe '#process' do
+    let(:runner) { described_class.new(build) }
+
+    context 'with success status' do
+      before do
+        expect(runner).to receive(:prepare_files).and_return(true)
+        expect(runner).to receive(:prepare_gems).and_return(true)
+        expect(runner).to receive(:run_specs).and_return(true)
+
+        expect(runner).to receive(:write_comment!)
+
+        runner.process
+      end
+
+      it { expect(build.reload).to be_success }
+    end
+
+    context 'with fail status' do
+      before do
+        expect(runner).to receive(:prepare_files).and_return(false)
+        expect(runner).to_not receive(:prepare_gems)
+        expect(runner).to_not receive(:run_specs)
+
+        expect(runner).to receive(:write_comment!)
+
+        runner.process
+      end
+
+      it { expect(build.reload).to be_fail }
+    end
+  end
+end
