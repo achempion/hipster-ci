f75884f929b4dc806d6bcb89576bf2a981c74f67
Boris Kuznetsov <achempion@gmail.com>
2014-10-22 17:45:43 +0400

    build runner use a build configuration

--- app/services/scheduler_service/buid_configuration.rb
+++ app/services/scheduler_service/buid_configuration.rb
@@ -1,42 +1,45 @@
-class BuildConfiguration
+module SchedulerService
+  class BuildConfiguration
 
-  #
-  # Configuration example
-  #
-  # /config/hipster_ci.yml
-  # ----------------------
-  # requirements:
-  #   - xfvb
-  #   - curl
-  #   - mysql-server
-  #
-  # spec_command: xvfb-run rspec
-  #
+    #
+    # Configuration example
+    #
+    # /config/hipster_ci.yml
+    # ----------------------
+    # requirements:
+    #   - xfvb
+    #   - curl
+    #   - mysql-server
+    #
+    # spec_command: xvfb-run rspec
+    #
 
-  def initialize build_folder
-    @build_folder = build_folder
-    @configuration = YAML.load_file build_folder.join('config', 'hipster_ci.yml')
-  end
-
-  def requirements
-    @configuration[:requirements]
-  end
-
-  def spec_command
-    @configuration[:spec_command]
-  end
-
-  def app_environment
-    'test'
-  end
-
-  private
-
-  def default_spec_command
-    if File.directory?(@build_folder.join('spec'))
-      'rspec'
-    else
-      'rake'
+    def initialize build_folder
+      @build_folder = build_folder
+      @configuration = YAML.load_file build_folder.join('config', 'hipster_ci.yml')
     end
+
+    def requirements
+      @configuration[:requirements]
+    end
+
+    def spec_command
+      @configuration[:spec_command] || default_spec_command
+    end
+
+    def test_environment
+      'test'
+    end
+
+    private
+
+    def default_spec_command
+      if File.directory?(@build_folder.join('spec'))
+        'rspec'
+      else
+        'rake'
+      end
+    end
+
   end
 end
--- app/services/scheduler_service/build_runner.rb
+++ app/services/scheduler_service/build_runner.rb
@@ -1,80 +1,85 @@
-class BuildRunner
-  def initialize build
-    @build = build
-  end
+module SchedulerService
+  class BuildRunner
 
-  def process
-    @build.in_progress!
-
-    @result = ''
-
-    okay = prepare_files && prepare_gems && run_specs
-
-    @build.result = @result
-
-    if okay
-      @build.success!
-    else
-      @build.fail!
+    def initialize build
+      @build = build
     end
 
-    notificate!
-  end
+    def process
+      @build.in_progress!
 
-  private
+      @result = ''
 
-  def prepare_files
-    `
-        rm -rf builds/#{@build.id} &&
-        mkdir -p builds/#{@build.id} &&
-        cd builds/#{@build.id} &&
-        git clone https://#{@build.project.access_token}@github.com/#{@build.project.path} . &&
-        git checkout #{@build.sha}
-    `
+      okay = prepare_files && prepare_gems && run_specs
 
-    `rm builds/#{@build.id}/config/database.yml`
-    `cp config/build_database.yml builds/#{@build.id}/config/database.yml`
+      @build.result = @result
 
-    true
-  end
-
-  def prepare_gems
-    bundle_status =
-      Bundler.with_clean_env do
-        system "cd builds/#{@build.id} && bundle > bundle_result 2>&1"
-      end
-
-    @result = `cd builds/#{@build.id} && cat bundle_result`
-
-    bundle_status
-  end
-
-  def run_specs
-    spec_command =
-      if File.directory?("builds/#{build.id}/spec")
-        'rspec'
+      if okay
+        @build.success!
       else
-        'rake'
+        @build.fail!
       end
 
-    spec_status =
-      Bundler.with_clean_env do
-        system "cd builds/#{@build.id} && RAILS_ENV=test bundle exec #{spec_command} > spec_result 2>&1"
-      end
+      notificate!
+    end
 
-    @result = `cd builds/#{@build.id} && cat spec_result`
+    private
 
-    spec_status
-  end
+    def build_folder
+      Rails.root.join('builds', @build.id)
+    end
 
-  def notificate!
-    if @build.project.notifications?(:github)
-      begin
-        GithubService.for_project(@build.project).create_comment(@build.sha, "**#{@build.status.humanize.upcase}**\n```\n#{@build.result}\n```")
-      rescue Octokit::Error => e
-        @build.result += "\n\nGITHUB EXCEPTION:\n#{e.message}"
-        @build.save
+    def build_configuration
+      @build_configuration ||= BuildConfiguration.new(build_folder)
+    end
+
+    def prepare_files
+      `
+          rm -rf #{build_folder} &&
+          mkdir -p #{build_folder} &&
+          cd #{build_folder} &&
+          git clone https://#{@build.project.access_token}@github.com/#{@build.project.path} . &&
+          git checkout #{@build.sha}
+      `
+
+      `rm #{ build_folder.join('config', 'database.yml') }`
+      `cp config/build_database.yml #{ build_folder.join('config', 'database.yml') }`
+
+      true
+    end
+
+    def prepare_gems
+      bundle_status =
+        Bundler.with_clean_env do
+          system "cd #{build_folder} && RAILS_ENV=#{build_configuration.test_environment} bundle > bundle_result 2>&1"
+        end
+
+      @result = File.read build_folder.join('bundle_result')
+
+      bundle_status
+    end
+
+    def run_specs
+      spec_status =
+        Bundler.with_clean_env do
+          system "cd #{build_folder} && RAILS_ENV=#{build_configuration.test_environment} bundle exec #{build_configuration.spec_command} > spec_result 2>&1"
+        end
+
+      @result = File.read build_folder.join('spec_result')
+
+      spec_status
+    end
+
+    def notificate!
+      if @build.project.notifications?(:github)
+        begin
+          GithubService.for_project(@build.project).create_comment(@build.sha, "**#{@build.status.humanize.upcase}**\n```\n#{@build.result}\n```")
+        rescue Octokit::Error => e
+          @build.result += "\n\nGITHUB EXCEPTION:\n#{e.message}"
+          @build.save
+        end
       end
     end
+
   end
 end
